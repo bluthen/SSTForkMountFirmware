1f1f75ae3a4343563aea35b5c02617254fb03794
Russell Valentine <russ@coldstonelabs.org>
2019-08-03 15:08:40 -0500

    client2 react start

---
+++ piside/client2/webpack.config.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'eval',
+    mode: 'development',
+    entry: [
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        "@babel/preset-react",
+                        "@babel/preset-env"
+                    ],
+                    plugins: [
+                        "@babel/plugin-transform-runtime",
+                        ["@babel/plugin-proposal-decorators", {"legacy": true}],
+                        "@babel/plugin-proposal-class-properties",
+                        "@babel/plugin-proposal-optional-chaining",
+                        ["@babel/plugin-proposal-pipeline-operator", {"proposal": "minimal"}]
+                    ]
+                },
+            },
+            include: path.join(__dirname, 'src')
+        }]
+    }
+};
