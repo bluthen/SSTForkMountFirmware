8004b8f27edcaafaea34e79e5d3558b470348056
Russell Valentine <russ@coldstonelabs.org>
2020-03-13 14:38:03 -0500

    version bump

--- piside/server/control.py
+++ piside/server/control.py
@@ -49,9 +49,9 @@ import skyconv
 import settings
 import motion
 
-version = "0.0.18"
+version = "0.0.19"
 version_short = "0.0"
-version_date_str = "Mar 04 2020"
+version_date_str = "Mar 13 2020"
 
 SIDEREAL_RATE = 0.004178074568511751  # 15.041"/s
 AXIS_RA = 1
